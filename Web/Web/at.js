9d1af01e9b12303db22d983e6642cf1e0044aa5b
ViniciusDaltro01 <viniciusprojetodax@gmail.com>
2025-06-30 11:23:43 -0300

    Add files via upload

---
+++ Web/Web/at.js
@@ -0,0 +1,7 @@
+function mostrar0la(){
+    var nome = document. getElementById("nome").value
+    document.getElementById("resposta").innerHTML = "ola"+ nome
+}
+
+var mostrar =document.getElementById("mostrar")
+mostrar.onclick=mostrar0la
